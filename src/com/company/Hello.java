72c44834693af13120893f6dc10c752baf0f27aa
Marina1862 <74152246+Marina1862@users.noreply.github.com>
2020-11-10 12:50:29 -0500

    Project is already remote repo...

--- src/com/company/Hello.java
+++ src/com/company/Hello.java
@@ -1,2 +1,8 @@
-package com.company;public class Hello {
+package com.company;
+
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
 }
